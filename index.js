ff096f215da63954392ba35952b5cf4a81ad4fdb
Mennz <davi.mendoza.rezende.c@gmail.com>
2026-09-24 13:25:39 -0300

    poe o menu no terminal

--- index.js
+++ index.js
@@ -1,2 +1,46 @@
-console.log("Caixa eletronico");
-console.log("carregando...");
+const readline = require("readline/promises");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function mostrarMenu() {
+  console.log("");
+  console.log("=== Caixa eletronico ===");
+  console.log("1 - Consultar saldo");
+  console.log("2 - Depositar");
+  console.log("3 - Sacar");
+  console.log("4 - Sair");
+}
+
+async function rodar() {
+  let rodando = true;
+
+  while (rodando) {
+    mostrarMenu();
+    const opcao = await rl.question("Escolha uma opcao: ");
+
+    switch (opcao.trim()) {
+      case "1":
+        console.log("ainda nao implementado");
+        break;
+      case "2":
+        console.log("ainda nao implementado");
+        break;
+      case "3":
+        console.log("ainda nao implementado");
+        break;
+      case "4":
+        console.log("ate mais!");
+        rodando = false;
+        break;
+      default:
+        console.log("opcao invalida");
+    }
+  }
+
+  rl.close();
+}
+
+rodar();
